450f70ce2e29aef20309e03db2fbc9e8ef386690
Mona Moxie <moxie4lyf@gmail.com>
2023-07-24 22:13:41 +0200

    use redis cache

--- api/app/Services/UserService.php
+++ api/app/Services/UserService.php
@@ -3,11 +3,19 @@
 namespace App\Services;
 
 use App\Models\User;
-
+use Illuminate\Support\Facades\Redis;
 class UserService
 {
     public function all()
     {
-        return User::with('weather')->get();
+        $users_data = Redis::get('users_data');
+        if(isset($users_data)) {
+            $users = json_decode($users_data);
+        } else {
+            $users = User::with('weather')->get();
+            Redis::set('users_data', $users, 'EX', 1800);
+        }
+
+        return $users;
     }
 }
